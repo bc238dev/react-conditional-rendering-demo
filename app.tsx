7c1970bc1458ab592abba345769ba4495697fb64
omt66 <omt66@yahoo.com>
2020-01-21 16:25:16 -0800

    added more panels for demo

--- app.tsx
+++ app.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { render } from "react-dom"
 import DemoComponent from "./demo-comp"
+import { PanelDemo1, PanelDemo2, PanelDemo3, InfoPanel, StatusKey } from "./panel-demo"
 
 export function App() {
   return (
@@ -10,6 +11,16 @@ export function App() {
       <DemoComponent panelName="panel1" />
       <DemoComponent panelName="panel2" />
       <DemoComponent panelName="panel3" />
+      <hr />
+      <h3>Panels:</h3>
+      <PanelDemo1 panelName="panel1" />
+      <PanelDemo2 panelName="panel2" />
+      <PanelDemo3 panelName="panel3" />
+      <hr />
+      Info Panel:
+      <InfoPanel statusKey={StatusKey.SUCCESS} info="Bravo, great job." />
+      <InfoPanel statusKey={StatusKey.WARNING} info="Something is not right..." />
+      <InfoPanel statusKey={StatusKey.ERROR} info="Opps. There is a problem!" />
     </div>
   )
 }
---
+++ panel-demo.tsx
@@ -0,0 +1,103 @@
+import React, { Component } from "react"
+
+export interface IProps {
+  panelName: string
+}
+
+export interface IPanelProps {
+  txt?: string
+}
+
+export class Panel1 extends Component<IPanelProps, {}> {
+  render() {
+    let { txt } = this.props
+    return <div className="panel1">{txt || "Panel1"}</div>
+  }
+}
+
+export class Panel2 extends Component<IPanelProps, {}> {
+  render() {
+    let { txt } = this.props
+    return <div className="panel2">{txt || "Panel2"}</div>
+  }
+}
+
+export class Panel3 extends Component<IPanelProps, {}> {
+  render() {
+    let { txt } = this.props
+    return <div className="panel3">{txt || "Panel3"}</div>
+  }
+}
+
+export class PanelDemo1 extends Component<IProps, {}> {
+  render() {
+    let { panelName } = this.props
+
+    return <div>{panelName === "panel1" ? <Panel1 /> : <Panel2 />}</div>
+  }
+}
+
+export class PanelDemo2 extends Component<IProps, {}> {
+  selectPanel = (panelName: string) => {
+    switch (panelName) {
+      case "panel1":
+        return <Panel1 />
+
+      case "panel2":
+        return <Panel2 />
+        break
+
+      default:
+        return <div>Unknown!</div>
+    }
+  }
+
+  render() {
+    let { panelName } = this.props
+    return this.selectPanel(panelName)
+  }
+}
+
+export class PanelDemo3 extends Component<IProps, {}> {
+  selectPanel = (panelName: string) => {
+    const map: { [key: string]: any } = {
+      panel1: <Panel1 />,
+      panel2: <Panel2 />,
+      panel3: <Panel3 />,
+    }
+    return map[panelName] || <Panel1 /> // Panel1 is our default panel!
+  }
+
+  render() {
+    let { panelName } = this.props
+    return this.selectPanel(panelName)
+  }
+}
+
+export enum StatusKey {
+  SUCCESS = "success",
+  WARNING = "warning",
+  ERROR = "error",
+}
+
+export interface IInfoProps {
+  statusKey: StatusKey
+  info: string
+}
+
+export class InfoPanel extends Component<IInfoProps, {}> {
+  selectPanel = (statusKey: StatusKey, info: string) => {
+    const defaultPanel = <Panel1 txt="Unknown" /> // Panel1 is our default panel!
+    const map: { [key: string]: any } = {
+      success: <Panel1 txt={info} />,
+      warning: <Panel2 txt={info} />,
+      error: <Panel3 txt={info} />,
+    }
+    return map[statusKey] || defaultPanel
+  }
+
+  render() {
+    let { statusKey, info } = this.props
+    return this.selectPanel(statusKey, info)
+  }
+}
